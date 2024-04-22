6e33b68bad78528d1d534f228ad141d13c4c351d
Natalya Ivanova <natalya.nata-cub@yandex.ru>
2024-04-22 21:55:17 +0300

    code added

---
+++ final_task/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Write("введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+string[] stringArray = new string[m];
+void array(string[] stringArray)
+{
+  for (int i = 0; i < stringArray.Length; i++)
+  {
+    Console.WriteLine($"введите {i + 1}элеиент массива");
+    stringArray[i] = Console.ReadLine()!;
+  }
+}
+string[] symbol(string[] stringArray)
+{
+  int n = 0;
+  for (int i = 0; i < stringArray.Length; i++)
+  {
+    if (stringArray[i].Length <= 3)
+      n++;
+  }
+  string[] result = new string[n];
+  int j = 0;
+  for (int i = 0; i < stringArray.Length; i++)
+  {
+    if (stringArray[i].Length <= 3)
+    {
+      result[j] = stringArray[i];
+      j++;
+    }
+  }
+  return result;
+}
+void printA(string[] stringArray)
+{
+  Console.Write("[");
+  for (int i = 0; i < stringArray.Length; i++)
+  {
+    Console.Write($"{stringArray[i]}");
+    Console.Write("  ");
+  }
+  Console.Write("]");
+}
+array(stringArray);
+printA(symbol(stringArray));
